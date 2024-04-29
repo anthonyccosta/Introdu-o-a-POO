79c25cbe1185b50c6305d8abab11113bd4ea5cbf
ccosta.anthony@gmail.com <ccosta.anthony@gmail.com>
2024-04-29 11:46:52 -0300

    Adicionar arquivos de projeto.

---
+++ Introdução a POO/Pessoa.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Pessoa
+{
+    public string Nome;
+    public string Sobrenome;
+    public int Idade;
+    public string Telefone;
+    public string Celular;
+    public string Email;
+
+    public Pessoa() //ctor abreviação para criar um construtor
+    {
+
+    }
+
+    public string DefinirNome()
+    {
+        Console.WriteLine("Informe o Nome: ");
+
+        //string nome;
+        //nome = Console.ReadLine();
+        //return nome; 
+
+        return Console.ReadLine();  // este meio é o mesmo do acima, mas encurtado
+
+    }
+
+    public string DefinirSobrenome()
+    {
+        Console.WriteLine("Informe o Sobrenome: ");
+
+        //string nome;
+        //nome = Console.ReadLine();
+        //return nome; 
+
+        return Console.ReadLine();  // este meio é o mesmo do acima, mas encurtado
+
+    }
+
+    public int DefinirIdade()
+    {
+        Console.WriteLine("Informe a idade: ");
+
+        //    int idade;
+        //    idade = int.Parse(Console.ReadLine());
+        //    return idade;
+
+        return int.Parse(Console.ReadLine()); // este meio é o mesmo do acima, mas encurtado 
+    }
+
+    public void DefinirCelular(string celular)
+    {
+        this.Celular = celular;
+        //Console.WriteLine("Informe o numero de Telefone desejado: ");
+
+        ////string nome;
+        ////nome = Console.ReadLine();
+        ////return nome; 
+
+        //return Console.ReadLine();  // este meio é o mesmo do acima, mas encurtado
+
+    }
+
+    public void DefinirTelefone(string telefone)
+    {
+        this.Telefone = telefone; //
+        //Console.WriteLine("Informe o numero de Telefone fixo desejado: ");
+
+        ////string nome;
+        ////nome = Console.ReadLine();
+        ////return nome; 
+
+        //return Console.ReadLine();  // este meio é o mesmo do acima, mas encurtado
+
+    }
+    public void DefinirEmail(string email)
+    {
+        this.Email = email;
+        //Console.WriteLine("Informe o E-mail desejado: ");
+
+        ////string nome;
+        ////nome = Console.ReadLine();
+        ////return nome; 
+
+        //return Console.ReadLine();  // este meio é o mesmo do acima, mas encurtado
+    }
+
+    public void MostrarDados()
+    {
+        Console.WriteLine($"Nome: {Nome}", this.Nome);
+        Console.WriteLine($"Sobrenome: {Sobrenome}", this.Sobrenome);
+        Console.WriteLine($"Idade: "+this.Idade);
+        Console.WriteLine($"Telefone: "+this.Telefone);
+        Console.WriteLine($"Celular: {Celular}", this.Celular);
+        Console.WriteLine($"Email: {Email}", this.Email);
+    }
+
+}
---
+++ Introdução a POO/Program.cs
@@ -0,0 +1,31 @@
+﻿// using Introdução_a_POO; //tenho que puxar aonde eu instanciei minha classe
+string telefone, celular, email;
+Pessoa pessoa = new Pessoa();
+
+pessoa.Nome = pessoa.DefinirNome(); // assim que aciona o metodo
+pessoa.Sobrenome = pessoa.DefinirSobrenome();
+pessoa.Idade = pessoa.DefinirIdade();
+
+
+Console.WriteLine("Informe o Telefone:");
+telefone = Console.ReadLine();
+pessoa.DefinirTelefone(telefone);
+
+Console.WriteLine("Informe o Celular:");
+celular = Console.ReadLine();
+pessoa.DefinirCelular(celular);
+
+Console.WriteLine("Informe o Email:");
+email = Console.ReadLine();
+pessoa.DefinirEmail(email);
+
+
+pessoa.MostrarDados();
+//Console.WriteLine(pessoa.ToString());
+
+//Console.WriteLine(pessoa.Nome);
+//Console.WriteLine(pessoa.Sobrenome);
+//Console.WriteLine(pessoa.Idade);
+//Console.WriteLine(pessoa.Telefone);
+//Console.WriteLine(pessoa.Celular);
+//Console.WriteLine(pessoa.Email);
