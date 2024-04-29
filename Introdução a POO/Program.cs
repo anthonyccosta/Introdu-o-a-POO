8c9987cd42e7a65c71ba5d06fab1be9fcb3ceec1
ccosta.anthony@gmail.com <ccosta.anthony@gmail.com>
2024-04-29 13:21:51 -0300

    feito acrescimo de mais 2 jogares

--- Introdução a POO/Program.cs
+++ Introdução a POO/Program.cs
@@ -1,26 +1,85 @@
 ﻿// using Introdução_a_POO; //tenho que puxar aonde eu instanciei minha classe
 string telefone, celular, email;
-Pessoa pessoa = new Pessoa();
 
-pessoa.Nome = pessoa.DefinirNome(); // assim que aciona o metodo
-pessoa.Sobrenome = pessoa.DefinirSobrenome();
-pessoa.Idade = pessoa.DefinirIdade();
+Pessoa pessoa1 = new Pessoa();
+Pessoa pessoa2 = new Pessoa();
+Pessoa pessoa3 = new Pessoa();
 
+Pessoa CriarPessoa()
+{
+Pessoa p = new Pessoa();
+
+#region pessoa1
+p.Nome = p.DefinirNome(); // assim que aciona o metodo
+p.Sobrenome = p.DefinirSobrenome();
+p.Idade = p.DefinirIdade();
 
 Console.WriteLine("Informe o Telefone:");
 telefone = Console.ReadLine();
-pessoa.DefinirTelefone(telefone);
+p.DefinirTelefone(telefone);
 
 Console.WriteLine("Informe o Celular:");
 celular = Console.ReadLine();
-pessoa.DefinirCelular(celular);
+p.DefinirCelular(celular);
 
 Console.WriteLine("Informe o Email:");
 email = Console.ReadLine();
-pessoa.DefinirEmail(email);
+p.DefinirEmail(email);
 
+return p;
+#endregion
+}
+pessoa1 = CriarPessoa();
+pessoa2 = CriarPessoa();
+pessoa3 = CriarPessoa();
 
-pessoa.MostrarDados();
+#region comentarios
+//pessoa2.Nome = pessoa2.DefinirNome(); // assim que aciona o metodo
+//pessoa2.Sobrenome = pessoa2.DefinirSobrenome();
+//pessoa2.Idade = pessoa2.DefinirIdade();
+
+//Console.WriteLine("Informe o Telefone:");
+//telefone = Console.ReadLine();
+//pessoa2.DefinirTelefone(telefone);
+
+//Console.WriteLine("Informe o Celular:");
+//celular = Console.ReadLine();
+//pessoa2.DefinirCelular(celular);
+
+//Console.WriteLine("Informe o Email:");
+//email = Console.ReadLine();
+//pessoa2.DefinirEmail(email);
+//pessoa2.MostrarDados();
+//#endregion
+
+//#region pessoa3
+//pessoa3.Nome = pessoa3.DefinirNome(); // assim que aciona o metodo
+//pessoa3.Sobrenome = pessoa3.DefinirSobrenome();
+//pessoa3.Idade = pessoa3.DefinirIdade();
+
+//Console.WriteLine("Informe o Telefone:");
+//telefone = Console.ReadLine();
+//pessoa3.DefinirTelefone(telefone);
+
+//Console.WriteLine("Informe o Celular:");
+//celular = Console.ReadLine();
+//pessoa3.DefinirCelular(celular);
+
+//Console.WriteLine("Informe o Email:");
+//email = Console.ReadLine();
+//pessoa3.DefinirEmail(email);
+#endregion
+
+Console.WriteLine("Primeiro Cadastro");
+pessoa1.MostrarDados();
+
+Console.WriteLine("Segundo Cadastro");
+pessoa2.MostrarDados();
+
+Console.WriteLine("Terceiro Cadastro");
+pessoa3.MostrarDados();
+
+#region comentarios
 //Console.WriteLine(pessoa.ToString());
 
 //Console.WriteLine(pessoa.Nome);
@@ -29,3 +88,4 @@ pessoa.MostrarDados();
 //Console.WriteLine(pessoa.Telefone);
 //Console.WriteLine(pessoa.Celular);
 //Console.WriteLine(pessoa.Email);
+#endregion
